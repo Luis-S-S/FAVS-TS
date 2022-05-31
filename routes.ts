a2b1f828fd1ea43138978934cb75d89bb7209e4c
Luis Salcedo Salas <rideluis@gmail.com>
2022-05-31 13:51:27 -0500

    feat: implement routes

--- routes.ts
+++ routes.ts
@@ -1,6 +1,10 @@
 import { Express } from 'express';
 import users from './api/users';
+import lists from './api/lists';
+import auth from './auth/local';
 
 export default function routes(app: Express) {
   app.use('/api/users', users);
+  app.use('/api/lists', lists);
+  app.use('/auth/local', auth);
 }
